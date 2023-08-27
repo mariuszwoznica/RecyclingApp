d60ec6e2e066b1e7b188572778b28b1a41c951c5
Mariusz Woźnica <mariusz.adam.woznica@gmail.com>
2023-08-27 23:19:38 +0200

    add command validator and tests

---
+++ src/RecyclingApp.Application/Products/Validators/Commands/DeleteProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using RecyclingApp.Application.Products.Commands;
+
+namespace RecyclingApp.Application.Products.Validators.Commands;
+
+public class DeleteProductValidator : AbstractValidator<DeleteProduct>
+{
+    public DeleteProductValidator()
+    {
+        RuleFor(x => x.ProductId)
+            .NotEmpty();
+    }
+}
