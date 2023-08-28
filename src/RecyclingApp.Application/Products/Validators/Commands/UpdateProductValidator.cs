48817f2dcfe286e5daabdedcd3c0a6f589696747
Mariusz Woźnica <mariusz.adam.woznica@gmail.com>
2023-08-28 16:02:54 +0200

    add command validator and validation tests

---
+++ src/RecyclingApp.Application/Products/Validators/Commands/UpdateProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using RecyclingApp.Application.Products.Commands;
+
+namespace RecyclingApp.Application.Products.Validators.Commands;
+
+public class UpdateProductValidator : AbstractValidator<UpdateProduct>
+{
+    public UpdateProductValidator()
+    {
+        RuleFor(x => x.ProductId)
+            .NotEmpty();
+
+        RuleFor(x => x.Type)
+            .NotEmpty();
+
+        RuleFor(x => x.Type)
+            .IsInEnum()
+            .WithMessage("Product type is not valid.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty();
+
+        RuleFor(x => x.Price)
+            .NotEmpty();
+    }
+}
